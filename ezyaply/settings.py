0e8e0a759e0f772ebdf24c1c67f353993cc64d25
JatinGoyal <56165691+Jatin-Goyal-552@users.noreply.github.com>
2021-11-07 16:25:14 +0530

    changed requirements.txt

--- ezyaply/settings.py
+++ ezyaply/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DEBUG = False
 
 
 # Quick-start development settings - unsuitable for production

7cf62739ea7315f64e66863978f3755b7e1f78df
JatinGoyal <56165691+Jatin-Goyal-552@users.noreply.github.com>
2021-11-07 15:52:13 +0530

    added assests folder

--- ezyaply/settings.py
+++ ezyaply/settings.py
@@ -132,9 +132,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, '/static')
+        os.path.join(BASE_DIR, 'static')
        ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assests')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True

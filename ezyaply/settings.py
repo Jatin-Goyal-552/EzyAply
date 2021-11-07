87ed450263f0831f42030982f45b7e9b5f68fee8
JatinGoyal <56165691+Jatin-Goyal-552@users.noreply.github.com>
2021-11-07 15:49:58 +0530

    changed static settings

--- ezyaply/settings.py
+++ ezyaply/settings.py
@@ -128,12 +128,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT ='/static/'
+# STATIC_ROOT ='/static/'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, '/static')
+       ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
